3e2a7a1dc8fc2081d00c1d158b2fc1b399d2f324
jaydensc-tsk <kungjayden0331@gmail.com>
2026-02-22 11:13:52 +0800

    edit examples

--- examples/example01.bat
+++ examples/example01.bat
@@ -1,4 +1,4 @@
 @echo off
 title sensei@anonaOS1.02.001-RTSM
 hostname
-cmd /k prompt sensei@anonaOS1.02.001-RTSM:$p$$$g && title sensei@anonaOS1.02.001-RTSM && hostname
+cmd /k prompt sensei@anonaOS1.02.001-RTSM:$p$$$g && hostname
